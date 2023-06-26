b47e11b5b119c4fc176139e58c9797acba6001c3
Ella <ellabanaticla@gmail.com>
2023-06-26 18:35:24 +1200

    Complete homepage styling

--- components/BrandNav.tsx
+++ components/BrandNav.tsx
@@ -4,6 +4,11 @@ const BrandNav = () => {
     return (
         <div className={styles.brandNavContainer}>
             <div className={styles.logo}></div>
+            <div className={styles.githubContainer}>
+                <a href='https://github.com/eggyolkk/lastMinute' target='/blank'>
+                    GitHub
+                </a>
+            </div>
         </div>
     )
 }
